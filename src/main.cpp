a18aa149c5da6c61e0e13fad765c69349ff5bafd
0xDATAWOLF <magiktrikdev@gmail.com>
2022-07-01 10:04:02 -0500

    Virtual Free & Reclaimation

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,26 @@ int main(int argc, char** argv)
 
 	// Allocate an array of integers.
 	int* intarr = (int*)smemory::alloc(sizeof(int)*numints);
+	int* intarr2 = (int*)smemory::alloc(sizeof(int)*numints);
+	void* largearr = smemory::alloc(smemory::page_size() * 2);
+	void* largearr2 = smemory::alloc(smemory::page_size() * 2);
+	void* largearr3 = smemory::alloc(smemory::page_size() * 2);
 
 	// Insert values into the array of integers.
 	for (int i = 0; i < numints; ++i) intarr[i] = i+1;
+	for (int i = 0; i < numints; ++i) intarr2[i] = i+1;
 
 	// Free those values.
 	smemory::free(intarr);
+
+	// Perform a reclaim.
+	smemory::reclaim();
+
+	// Free the second array.
+	smemory::free(intarr2);
+
+	// Attempt another reclaim. This should reclaim the region.
+	smemory::reclaim();
 	
 }
 
